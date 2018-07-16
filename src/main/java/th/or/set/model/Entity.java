7b823d26975d3cc72695f164161f473fd9e92ba6
Pratz <pratz.nud@gmail.com>
2018-07-17 06:04:59 +0700

    add validator

---
+++ src/main/java/th/or/set/model/Entity.java
@@ -0,0 +1,18 @@
+package th.or.set.model;
+
+import java.math.BigDecimal;
+
+public class Entity {
+	private BigDecimal price;
+	
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	
+
+}
